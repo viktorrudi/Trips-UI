9940cd95c8df1d23ef077ce3a885b5a493decb32
Viktor Rudi <vrudi91@gmail.com>
2022-10-06 12:32:27 +0200

    Add Trip component

---
+++ my-app/src/components/Trip.tsx
@@ -0,0 +1,27 @@
+import {
+  getEmissionsOffset,
+  getImage,
+  getName,
+  getRating,
+  Trip as TripEntity,
+} from "../features/trips/tripsEntity";
+import { getTripSubtitle } from "../features/trips/tripsPresenter";
+import { Card } from "../ui/Card/Card";
+
+export function Trip(trip: TripEntity) {
+  const subtitle = getTripSubtitle(trip);
+
+  return (
+    <Card backgroundImage={getImage(trip)}>
+      <Card.Content>
+        <Card.Title>{getName(trip)}</Card.Title>
+        <Card.Subtitle>{subtitle}</Card.Subtitle>
+        <Card.Snack
+          label="Emissions offset"
+          value={`${getEmissionsOffset(trip)} t CO₂e`}
+        />
+      </Card.Content>
+      <Card.Rating title="Trip rating" rate={getRating(trip)} />
+    </Card>
+  );
+}
