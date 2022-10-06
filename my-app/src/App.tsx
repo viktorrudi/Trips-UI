ae6ddb0d55e1b84ac11e5cd9aa404b43a7e413bb
Viktor Rudi <vrudi91@gmail.com>
2022-10-06 10:43:05 +0200

    Add Shell and Trips components to App

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,24 +1,14 @@
 import "./App.css";
+import { Trips } from "./components/Trips";
 import { Providers } from "./global/Providers";
+import { Shell } from "./ui/Shell/Shell";
 
 function App() {
   return (
     <Providers>
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Shell>
+        <Trips />
+      </Shell>
     </Providers>
   );
 }
