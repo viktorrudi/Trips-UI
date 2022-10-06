10bf22595f2eea847b2ac6959b05048bd3d79af5
Viktor Rudi <vrudi91@gmail.com>
2022-10-06 12:12:00 +0200

    Fix grid size

--- my-app/src/ui/Grid/Grid.tsx
+++ my-app/src/ui/Grid/Grid.tsx
@@ -6,7 +6,7 @@ export function CardGrid({ children }: CardGridProps) {
   return (
     <SimpleGrid
       className="CardGrid"
-      columns={{ sm: 2, md: 3 }}
+      columns={{ sm: 1, md: 2, xl: 3 }}
       spacing="8"
       p="10"
     >
