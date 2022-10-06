0704d2bf5a326680ec143a1e4d8245fb2e6feba2
Viktor Rudi <vrudi91@gmail.com>
2022-10-06 10:42:52 +0200

    Add Trips component

---
+++ my-app/src/components/Trips.tsx
@@ -0,0 +1,21 @@
+import { Card } from "../ui/Card/Card";
+import { CardGrid } from "../ui/Grid/Grid";
+
+export function Trips() {
+  return (
+    <CardGrid>
+      <Card>
+        <h2>Card 1</h2>
+      </Card>
+      <Card>
+        <h2>Card 2</h2>
+      </Card>
+      <Card>
+        <h2>Card 3</h2>
+      </Card>
+      <Card>
+        <h2>Card 4</h2>
+      </Card>
+    </CardGrid>
+  );
+}
