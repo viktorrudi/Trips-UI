cbfe42c6f2d4b45ce950bc6defc8dc27c8333d15
Viktor Rudi <vrudi91@gmail.com>
2022-10-06 12:12:59 +0200

    Add card components

--- my-app/src/ui/Card/Card.tsx
+++ my-app/src/ui/Card/Card.tsx
@@ -1,59 +1,98 @@
-import {
-  Box,
-  BoxProps,
-  Heading,
-  HeadingProps,
-  TextProps,
-  Text,
-} from "@chakra-ui/react";
+import { Box, Heading, HeadingProps, TextProps, Text } from "@chakra-ui/react";
+import { Rating, RatingProps } from "../Rating/Rating";
 import { Snack } from "../Snack/Snack";
 
-type CardProps = BoxProps & {
+import "./Card.style.scss";
+
+type CardProps = {
   backgroundImage: string;
-  children: JSX.Element;
+  children: JSX.Element[];
 };
 
-function Card({ className = "", backgroundImage, ...props }: CardProps) {
+function Card({ backgroundImage, ...props }: CardProps) {
   return (
     <Box
-      backgroundImage={backgroundImage}
       className="Card"
+      backgroundImage={backgroundImage}
+      backgroundSize="cover"
+      backgroundPosition="center"
       boxShadow="xs"
       p="6"
-      rounded="md"
-      bg="white"
       {...props}
     />
   );
 }
 
-function Title(props: HeadingProps) {
-  return <Heading {...props} />;
+type ContentProps = { children: JSX.Element[] };
+function CardContent(props: ContentProps) {
+  return <Box className="Card__Content" {...props} />;
 }
 
-function Subtitle(props: TextProps) {
-  return <Text {...props} />;
+function CardTitle(props: HeadingProps) {
+  return (
+    <Heading
+      size="md"
+      textAlign="center"
+      color="white"
+      className="Card__Title"
+      {...props}
+    />
+  );
 }
 
-type AttentionBlockProps = {
+function CardSubtitle(props: TextProps) {
+  return (
+    <Text
+      fontSize="xs"
+      textAlign="center"
+      color="white"
+      className="Card__Subtitle"
+      {...props}
+    />
+  );
+}
+
+type CardSnackProps = {
   label: string;
   value: string;
 };
-function Attention({ label, value }: AttentionBlockProps) {
+function CardSnack({ label, value }: CardSnackProps) {
   return (
-    <Snack className="AttentionBlock">
+    <Snack className="Card__Snack">
       <Snack.Head>
-        <Snack.Text>{label}</Snack.Text>
+        <Snack.Text fontSize="sm">{label}</Snack.Text>
       </Snack.Head>
       <Snack.Tail>
-        <Snack.Text>{value}</Snack.Text>
+        <Snack.Text fontSize="sm">{value}</Snack.Text>
       </Snack.Tail>
     </Snack>
   );
 }
 
-Card.Title = Title;
-Card.Subtitle = Subtitle;
-Card.AttentionBlock = Attention;
+type CardRatingProps = RatingProps & {
+  title: string;
+  rate: RatingProps["initialValue"];
+};
+function CardRating({ title, rate }: CardRatingProps) {
+  return (
+    <Box className="Card__Rating" bg="white">
+      <Text fontSize="xs" className="Rating__Title">
+        {title}
+      </Text>
+      <Box className="Rating__Stars">
+        <Rating size={30} initialValue={rate} />
+      </Box>
+      <Text fontWeight="bold" className="Rating__Rate">
+        {rate}
+      </Text>
+    </Box>
+  );
+}
+
+Card.Content = CardContent;
+Card.Title = CardTitle;
+Card.Subtitle = CardSubtitle;
+Card.Snack = CardSnack;
+Card.Rating = CardRating;
 
 export { Card };
