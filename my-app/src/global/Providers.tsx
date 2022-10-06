0c753c07a5acebe1545cd087f8f50981189f52a8
Viktor Rudi <vrudi91@gmail.com>
2022-10-06 12:51:40 +0200

    Add queryclient provider

--- my-app/src/global/Providers.tsx
+++ my-app/src/global/Providers.tsx
@@ -1,7 +1,14 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 type ProviderProps = { children: JSX.Element };
 
+const queryClient = new QueryClient();
+
 export function Providers({ children }: ProviderProps) {
-  return <ChakraProvider>{children}</ChakraProvider>;
+  return (
+    <ChakraProvider>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </ChakraProvider>
+  );
 }
