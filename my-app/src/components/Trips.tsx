f1176d75a40ac984ec44a9171173198d867e5b6b
Viktor Rudi <vrudi91@gmail.com>
2022-10-06 12:13:22 +0200

    Add sample data to Trips

--- my-app/src/components/Trips.tsx
+++ my-app/src/components/Trips.tsx
@@ -4,17 +4,55 @@ import { CardGrid } from "../ui/Grid/Grid";
 export function Trips() {
   return (
     <CardGrid>
-      <Card>
-        <h2>Card 1</h2>
+      <Card backgroundImage="https://picsum.photos/id/237/300">
+        <Card.Content>
+          <Card.Title>Diving adventure in Egypt</Card.Title>
+          <Card.Subtitle>2 countries, 5 days</Card.Subtitle>
+          <Card.Snack label="Emissions offset" value="3.56 t CO₂e" />
+        </Card.Content>
+        <Card.Rating title="Trip rating" rate={4} />
       </Card>
-      <Card>
-        <h2>Card 2</h2>
+
+      <Card backgroundImage="https://picsum.photos/id/247/300">
+        <Card.Content>
+          <Card.Title>Diving adventure in Egypt</Card.Title>
+          <Card.Subtitle>2 countries, 5 days</Card.Subtitle>
+          <Card.Snack label="Emissions offset" value="3.56 t CO₂e" />
+        </Card.Content>
+        <Card.Rating title="Trip rating" rate={4} />
       </Card>
-      <Card>
-        <h2>Card 3</h2>
+
+      <Card backgroundImage="https://picsum.photos/id/277/300">
+        <Card.Content>
+          <Card.Title>Diving adventure in Egypt</Card.Title>
+          <Card.Subtitle>2 countries, 5 days</Card.Subtitle>
+          <Card.Snack label="Emissions offset" value="3.56 t CO₂e" />
+        </Card.Content>
+        <Card.Rating title="Trip rating" rate={4} />
       </Card>
-      <Card>
-        <h2>Card 4</h2>
+      <Card backgroundImage="https://picsum.photos/id/277/300">
+        <Card.Content>
+          <Card.Title>Diving adventure in Egypt</Card.Title>
+          <Card.Subtitle>2 countries, 5 days</Card.Subtitle>
+          <Card.Snack label="Emissions offset" value="3.56 t CO₂e" />
+        </Card.Content>
+        <Card.Rating title="Trip rating" rate={4} />
+      </Card>
+      <Card backgroundImage="https://picsum.photos/id/277/300">
+        <Card.Content>
+          <Card.Title>Diving adventure in Egypt</Card.Title>
+          <Card.Subtitle>2 countries, 5 days</Card.Subtitle>
+          <Card.Snack label="Emissions offset" value="3.56 t CO₂e" />
+        </Card.Content>
+        <Card.Rating title="Trip rating" rate={4} />
+      </Card>
+      <Card backgroundImage="https://picsum.photos/id/277/300">
+        <Card.Content>
+          <Card.Title>Diving adventure in Egypt</Card.Title>
+          <Card.Subtitle>2 countries, 5 days</Card.Subtitle>
+          <Card.Snack label="Emissions offset" value="3.56 t CO₂e" />
+        </Card.Content>
+        <Card.Rating title="Trip rating" rate={4} />
       </Card>
     </CardGrid>
   );
